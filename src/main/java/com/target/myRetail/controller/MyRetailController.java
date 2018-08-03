f37a58bffdf7930b4c1cbb67019bd7aa49af2b8c
Tania90 <Tania90@192.168.0.7>
2018-08-02 21:47:53 -0500

    "P13 Configuring same endpoint for get and put"

--- src/main/java/com/target/myRetail/controller/MyRetailController.java
+++ src/main/java/com/target/myRetail/controller/MyRetailController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,25 +31,29 @@ public class MyRetailController {
 	@Autowired
 	PriceDao priceDao;
 
-	@ApiOperation(value = "View a list of available products", response = Response.class)
+	@ApiOperation(value = "View or update product info ", response = Response.class)
 	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Successfully retrieved list"),
+			@ApiResponse(code = 200, message = "Successfully retrieved/updated list"),
 			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
 			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
 			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
-	@RequestMapping(path = "/products/{productID}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public Response getResponseForProductId(@PathVariable int productID)
+	@RequestMapping(path = "/products/{productID}", method = {
+			RequestMethod.GET, RequestMethod.PUT }, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Response getResponseForProductId(@PathVariable int productID,
+			@RequestParam(required = false) ProductPricing productPricing)
 			throws JSONException, IOException {
 		Response response = Response.builder().build();
+		if (productPricing != null) {
+			updateProductPricing(productPricing);
+		} else {
 
-		response = productInfoService.getProductInfo(productID);
+			response = productInfoService.getProductInfo(productID);
+		}
 		return response;
 	}
 
 	@ApiOperation(value = "Add a product info")
-	@RequestMapping(path = "/create", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@ResponseBody
-	public void createProductPricing(@RequestBody ProductPricing productPricing) {
+	public void updateProductPricing(ProductPricing productPricing) {
 		priceDao.create(productPricing);
 
 	}
